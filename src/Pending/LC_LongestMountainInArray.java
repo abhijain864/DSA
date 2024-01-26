b762576bdb96f0150e001d427090f7992fa52c3d
Abhishek Jain <abhishekjain@C02HFCTYQ6L4.local>
2024-01-27 00:30:38 +0530

    random

--- src/Pending/LC_LongestMountainInArray.java
+++ src/Pending/LC_LongestMountainInArray.java
@@ -15,5 +15,6 @@ public class LC_LongestMountainInArray {
 
     public static void main(String[] args) {
         System.out.println(new LC_LongestMountainInArray().longestMountain(new int[]{2,2,2}));
+        System.out.println("sdjnfjnjfn");
     }
 }
